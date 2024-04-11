1a697aabf3095fbc6c1d1096e56a2efa674fc153
ericayen <ahnuyan@gmail.com>
2024-04-11 11:52:33 -0700

    added new poem and image

--- script.js
+++ script.js
@@ -13,7 +13,12 @@ const poems = [
     author: "Ernie",
     poem: "Roses are red \n Violets are blue \n Jerry Seinfeld is considering \n Making Bee Movie 2",
     image: "bee.jpeg",
-  },  
+  },
+  {
+    author: "Erica",
+    poem: "Roses are red \n Violets are blue \n I have no idea what I'm doing \n Peanut Butter",
+    image: "peanut-butter.jpeg",
+  },
 ];
 
 let displayPoems = (array) => {
