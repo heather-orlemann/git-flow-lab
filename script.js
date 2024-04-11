c7d4dac3059afcde905617de2a2a1d06bc8653ad
Elina Kim <elinakim791@gmail.com>
2024-04-11 11:51:46 -0700

    add poem and change background color

--- script.js
+++ script.js
@@ -14,6 +14,11 @@ const poems = [
     poem: "Roses are red \n Violets are blue \n Jerry Seinfeld is considering \n Making Bee Movie 2",
     image: "bee.jpeg",
   },  
+  {
+    author: "Elina",
+    poem: "Roses Are Red",
+    image: "rosesarered.jpg",
+  },
 ];
 
 let displayPoems = (array) => {
